68abebfe2ca534eb75d7420c7d4ae3168053bae5
Scott Smith <scottsmit@gmail.com>
2017-12-14 17:49:14 -0800

    playing

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,20 +1,56 @@
 const { environment } = require('@rails/webpacker')
 const erb = environment.loaders.get('erb')
 
-// Re-insert back again but before babel loader
-environment.loaders.insert('erb', erb, { before: 'babel' })
+const newErb = {
+  test: /\.erb$/,
+  enforce: 'pre',
+  loader: 'rails-erb-loader',
+  exclude: /node_modules/,
+  // test: /\.erb$/,
+  // enforce: 'pre',
+  // use: [ { loader: 'rails-erb-loader', options: [Object] } ]
+}
 
-console.log(environment)
-environment.config.merge({
-  module: {
-    rules: [
-      environment.loaders.get('erb')
-    ]
-  }
-})
+// // console.log(environment.loaders)
+// // Re-insert back again but before babel loader
+//
+// console.log(erb)
+environment.loaders.insert('erb', newErb, { before: 'babel' })
 
-environment.loaders.delete('erb')
+console.log(environment.loaders)
+// module.exports = {
+//     module: {
+//       rules: [
+//         {
+//           test: /\.erb$/,
+//           enforce: 'pre',
+//           loader: 'rails-erb-loader'
+//         },
+//       ]
+//     }
+//   }
+// };
+
+//
+// console.log(environment)
+// environment.config.merge({
+//   module: {
+//     rules: [
+//       {
+//         test: /\.erb$/,
+//         enforce: 'pre',
+//         loader: 'rails-erb-loader'
+//       },
+//     ]
+//   }
+// })
+
+// { test: /\.erb$/,
+//   enforce: 'pre',
+//   exclude: /node_modules/,
+//   use: [ { loader: 'rails-erb-loader', options: [Object] } ] }
+
+
+// environment.loaders.delete('erb')
 
 module.exports = environment
-
-
