14b0de8be437657b41786362318c3dd479c16237
Scott Smith <scottsmit@gmail.com>
2017-12-15 13:42:52 -0800

    never got erb wroking

--- app/javascript/packs/home.js
+++ app/javascript/packs/home.js
@@ -9,23 +9,25 @@ import '../src/application.scss';
 // https://github.com/rails/webpacker/issues/1081
 
 export class Home extends React.Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+  }
 
   render(){
     return (
       <div className="top-bar">
         <div className="top-bar-left">
           <ul className="menu">
-            <li><img src="<%= asset_pack_path('images/super.png') %>" /></li>
             <li className="menu-text"> Hello {this.props.name}, whatssss up!</li>
             <li><a href="#">One</a></li>
             <li><a href="#">Two</a></li>
             <li><a href="#">Three</a></li>
+            <li><a href="#">Four</a></li>
           </ul>
         </div>
       </div>
     );
   }
 }
+
+
--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,23 +1,29 @@
 const { environment } = require('@rails/webpacker')
-const erb = environment.loaders.get('erb')
-
-const newErb = {
-  test: /\.erb$/,
-  enforce: 'pre',
-  loader: 'rails-erb-loader',
-  exclude: /node_modules/,
-  // test: /\.erb$/,
-  // enforce: 'pre',
-  // use: [ { loader: 'rails-erb-loader', options: [Object] } ]
-}
-
-// // console.log(environment.loaders)
-// // Re-insert back again but before babel loader
+// console.log(environment.loaders)
+// const erb = environment.loaders.get('erb')
+// const newErb = {
+//   test: /\.erb$/,
+//   enforce: 'pre',
+//   loader: 'rails-erb-loader',
+//   exclude: /node_modules/,
+//   // test: /\.erb$/,
+//   // enforce: 'pre',
+//   // use: [ { loader: 'rails-erb-loader', options: [Object] } ]
+// }
 //
-// console.log(erb)
-environment.loaders.insert('erb', newErb, { before: 'babel' })
+// // // console.log(environment.loaders)
+// // // Re-insert back again but before babel loader
+// //
+// console.log(environment.loaders)
+// environment.loaders.insert('erb', erb, { before: 'babel' })
+// const bable = environment.loaders.get('babel')
+// bable.query =  { presets: ['es2015']}
+//
+// environment.loaders.insert('erb', erb, { before: 'babel' })
 
-console.log(environment.loaders)
+console.log(environment.loaders.get('babel'))
+console.log('--------------------')
+console.log(environment)
 // module.exports = {
 //     module: {
 //       rules: [
